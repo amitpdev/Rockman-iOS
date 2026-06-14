d27f719ccfd8c986131d0c9d696b152cfd4889c0
Amit Palomo <apalgm@gmail.com>
2026-06-14 23:32:34 +0300

    Initial Commit

---
+++ RockMan/RockManApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockManApp.swift
+//  RockMan
+//
+//  Created by Amit Palomo on 14/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct RockManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RockManTests/RockManTests.swift
@@ -0,0 +1,18 @@
+//
+//  RockManTests.swift
+//  RockManTests
+//
+//  Created by Amit Palomo on 14/06/2026.
+//
+
+import Testing
+
+struct RockManTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
